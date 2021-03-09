a1ef396264ba52eb2890563c47dac7353594eaa1
Paul Ladd <vlendle@gmail.com>
2021-03-09 18:31:07 +0000

    Single spin animation functioning

---
+++ spinner.js
@@ -0,0 +1,11 @@
+const spinner = () => {
+  const spinChars = ["|", "/", "—", "\\"];
+  let delay = 500;
+  for (const char of spinChars) { 
+    setTimeout( () => process.stdout.write("\r" + char), delay)
+    delay += 500;
+  }
+  process.stdout.write("\n");
+};
+
+spinner();
